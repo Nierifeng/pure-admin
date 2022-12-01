1b42eec302fd932abce080512b9c19c09f632b6f
聂日锋 <17673376360@163.com>
2022-12-01 10:35:39 +0800

    修改vscode配置

--- build/info.ts
+++ build/info.ts
@@ -1,8 +1,8 @@
-import type { Plugin } from "vite";
-import dayjs, { Dayjs } from "dayjs";
-import duration from "dayjs/plugin/duration";
-import { green, blue, bold } from "picocolors";
-import { getPackageSize } from "@pureadmin/utils";
+import { getPackageSize } from '@pureadmin/utils';
+import dayjs, { Dayjs } from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+import { bold, green } from 'picocolors';
+import type { Plugin } from 'vite';
 dayjs.extend(duration);
 
 export function viteBuildInfo(): Plugin {
@@ -10,26 +10,17 @@ export function viteBuildInfo(): Plugin {
   let startTime: Dayjs;
   let endTime: Dayjs;
   return {
-    name: "vite:buildInfo",
+    name: 'vite:buildInfo',
     configResolved(resolvedConfig: { command: string }) {
       config = resolvedConfig;
     },
     buildStart() {
-      console.log(
-        bold(
-          green(
-            `👏欢迎使用${blue(
-              "[vue-pure-admin]"
-            )}，如果您感觉不错，记得点击后面链接给个star哦💖 https://github.com/xiaoxian521/vue-pure-admin`
-          )
-        )
-      );
-      if (config.command === "build") {
+      if (config.command === 'build') {
         startTime = dayjs(new Date());
       }
     },
     closeBundle() {
-      if (config.command === "build") {
+      if (config.command === 'build') {
         endTime = dayjs(new Date());
         getPackageSize({
           callback: (size: string) => {
@@ -38,7 +29,7 @@ export function viteBuildInfo(): Plugin {
                 green(
                   `🎉恭喜打包完成（总用时${dayjs
                     .duration(endTime.diff(startTime))
-                    .format("mm分ss秒")}，打包后的大小为${size}）`
+                    .format('mm分ss秒')}，打包后的大小为${size}）`
                 )
               )
             );
